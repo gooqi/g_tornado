06a491639a8d7cec6472ef6a4b71558ae8680658
jeff.jianbo.zhu <jeff.jianbo.zhu@accenture.com>
2019-01-14 03:12:29 +0800

    add siv

--- urls.py
+++ urls.py
@@ -1,14 +1,6 @@
 #coding:utf-8
 from handlers.index import MainHandler
-from handlers.upload import UploadHandler
-from handlers.websocket import WebSocketHandler
-from handlers.login import LoginHandler
-from handlers.room import RoomHandler
 
 urls = [
 (r'/', MainHandler),
-(r'/upload',UploadHandler),
-(r'/websocket', WebSocketHandler),
-(r'/onLogin', LoginHandler),
-(r'/room', RoomHandler),
 ]
